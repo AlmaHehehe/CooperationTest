b38b6b5c92ad9e02710c11f1f60411daf106c887
LXHe <lingxiao.he@hotmail.com>
2022-09-08 08:14:00 +0800

    There is an error in building variable b I have fixed that.

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import numpy as np
 a = np.random.randint(1, 3, (2,3))
 print(a)
 print('This is a cooperation test')
-b = np.random.randint(3, (2,3))
+b = np.random.randint(2, 3, (2,3))
 
 c = a.T @ b
 print(c)
